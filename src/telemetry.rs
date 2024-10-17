64d55ed2f42775836eb73b836daa4a25fd2d1821
Tergel M <tergel.m@cody.mn>
2024-10-17 12:20:32 +0800

    feat(core): logging test impr

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -23,22 +23,6 @@ where
         .with(env_filter)
         .with(JsonStorageLayer)
         .with(formatting_layer)
-
-    // tracing_subscriber::registry()
-    //     .with(
-    //         tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
-    //             // axum logs rejections from built-in extractors with the `axum::rejection`
-    //             // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
-    //             format!(
-    //                 "{}=info,tower_http=info,axum::rejection=trace",
-    //                 env!("CARGO_CRATE_NAME")
-    //             )
-    //             .into()
-    //         }),
-    //     )
-    //     .with(JsonStorageLayer)
-    //     .with(formatting_layer)
-    //     .init();
 }
 
 /// Register telemetry as global default to process span data.
