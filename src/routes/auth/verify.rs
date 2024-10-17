5504a4e312ba78773e06809388f7b76ed9025678
Tergel M <tergel.m@cody.mn>
2024-10-17 17:55:13 +0800

    feat(auth): verify email init

--- src/routes/auth/verify.rs
+++ src/routes/auth/verify.rs
@@ -1,20 +1,20 @@
-use axum::{extract::State, routing::post, Router};
-use secrecy::SecretString;
+use anyhow::Context;
+use axum::extract::State;
 use serde::Deserialize;
+use sqlx::PgPool;
 use utoipa::ToSchema;
 use validator::Validate;
 
 use crate::app::{
     error::AppError,
     extrator::{AuthUser, ValidatedJson},
-    otp::{email_otp::EmailVerifyOtp, OtpManager},
+    otp::email_otp::EmailVerifyOtp,
     ApiContext,
 };
 
 #[derive(Debug, Deserialize, Validate, ToSchema)]
 pub struct VerifyEmailInput {
-    #[schema(value_type = String)]
-    token: SecretString,
+    token: String,
 }
 
 #[utoipa::path(
@@ -28,6 +28,7 @@ pub struct VerifyEmailInput {
         (status = 200, description = "Successful created"),
         (status = 400, description = "Bad request"),
         (status = 401, description = "Unauthorized"),
+        (status = 404, description = "Not found"),
         (status = 422, description = "Invalid input", body = AppError),
         (status = 500, description = "Internal server error")
     )
@@ -38,5 +39,34 @@ pub async fn verify_email(
     ctx: State<ApiContext>,
     ValidatedJson(req): ValidatedJson<VerifyEmailInput>,
 ) -> Result<(), AppError> {
+    let otp_manager = EmailVerifyOtp {
+        user_id: auth_user.user_id,
+    };
+
+    let email_to_verify = otp_manager.get_data(&req.token, &ctx.redis_client).await?;
+
+    match email_to_verify {
+        Some(email) => {
+            update_email_status_to_verified(&email, &ctx.db_pool).await?;
+
+            Ok(())
+        }
+        None => Err(AppError::NotFound),
+    }
+}
+
+async fn update_email_status_to_verified(email: &str, pool: &PgPool) -> anyhow::Result<()> {
+    let _ = sqlx::query!(
+        r#"
+            update email 
+            set verified = true
+            where email = $1                
+        "#,
+        email
+    )
+    .execute(pool)
+    .await
+    .context("failed to set email to verified");
+
     Ok(())
 }
