df9527c6f5282766c1f02c40d056b14c36be4025
Tergel M <tergel.m@cody.mn>
2025-01-07 15:55:40 +0800

    refactor: fixed extra page

--- src/routes/admin/users.rs
+++ src/routes/admin/users.rs
@@ -58,34 +58,37 @@ pub async fn list_users(
     ctx: State<ApiContext>,
     Query(req): Query<PaginationInput>,
 ) -> Result<Json<UserListResponse>, AppError> {
-    let page_size: i64 = 25;
+    let page_size: usize = 25;
+    let cursor_size: i64 = (page_size + 1) as i64;
+
     match req.cursor {
         Some(cursor) => {
-            let next_res = sqlx::query_as!(
+            let mut next_res = sqlx::query_as!(
                 UserData,
                 r#"
                     select u.user_id, u.username, u.created_at
                     from "user" u
-                    where (created_at, user_id) < ($1, $2)
+                    where (created_at, user_id) <= ($1, $2)
                     order by created_at desc, user_id desc
                     limit $3
                 "#,
                 cursor.created_at.0,
                 cursor.id,
-                page_size
+                cursor_size
             )
             .fetch_all(&*ctx.db_pool)
             .await?;
 
-            let next_cursor: Option<CPagination> = if next_res.len() < page_size.try_into().unwrap()
-            {
-                None
-            } else {
-                next_res.last().map(|item| CPagination {
-                    id: item.user_id,
-                    created_at: item.created_at.clone(),
-                })
-            };
+            let next_cursor: Option<CPagination> =
+                if next_res.len() < cursor_size.try_into().unwrap() {
+                    None
+                } else {
+                    let next_item = next_res.pop();
+                    next_item.map(|item| CPagination {
+                        id: item.user_id,
+                        created_at: item.created_at.clone(),
+                    })
+                };
 
             return Ok(Json(UserListResponse {
                 data: next_res,
@@ -94,7 +97,7 @@ pub async fn list_users(
         }
         None => {
             // When there's no cursor, just fetch the first 25 rows
-            let next_res = sqlx::query_as!(
+            let mut next_res = sqlx::query_as!(
                 UserData,
                 r#"
                     select u.user_id, u.username, u.created_at
@@ -102,20 +105,21 @@ pub async fn list_users(
                     order by created_at desc, user_id desc
                     limit $1
                 "#,
-                page_size
+                cursor_size
             )
             .fetch_all(&*ctx.db_pool)
             .await?;
 
-            let next_cursor: Option<CPagination> = if next_res.len() < page_size.try_into().unwrap()
-            {
-                None
-            } else {
-                next_res.last().map(|item| CPagination {
-                    id: item.user_id,
-                    created_at: item.created_at.clone(),
-                })
-            };
+            let next_cursor: Option<CPagination> =
+                if next_res.len() < cursor_size.try_into().unwrap() {
+                    None
+                } else {
+                    let next_item = next_res.pop();
+                    next_item.map(|item| CPagination {
+                        id: item.user_id,
+                        created_at: item.created_at.clone(),
+                    })
+                };
 
             return Ok(Json(UserListResponse {
                 data: next_res,
