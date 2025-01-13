887b8ba7ee7e237b5f1118a3c7994cfafe384c39
Tergel M <tergel.m@cody.mn>
2025-01-13 16:44:47 +0800

    feat: address table init

--- migrations/20250113021312_address_table.sql
+++ migrations/20250113021312_address_table.sql
@@ -1,7 +1,6 @@
 create extension postgis;
 
 create type addr_area_type as enum ('Country', 'Province', 'City', 'District');
--- create type addr_el_type as enum ('StreetName', 'BuildingNumber', 'PostalCode', 'Unit', 'Floor');
 
 create table address_area
 (
@@ -12,13 +11,21 @@ create table address_area
 
   created_at  timestamptz not null default now(),
   updated_at  timestamptz
-
 );
 
 select trigger_updated_at('address_area');
-select add_hstore_not_null('address_area', 'name'); 
+select add_hstore_not_null('address_area', 'name');
 
--- Country: Mongolia
--- Province: Tuv
--- City: Ulaanbaatar
--- District: Bagakhangai, Baganuur, Bayangol etc...
+create table address
+(
+  address_id uuid primary key default uuid_generate_v1mc(),
+  line_1 hstore not null, -- for display
+  line_2 hstore, -- more info
+  geom geometry(point, 4326),  -- For GIS coordinates
+  created_at  timestamptz not null default now(),
+  updated_at  timestamptz
+);
+
+select trigger_updated_at('address');
+select add_hstore_not_null('address', 'line_1');
+select add_hstore_not_null('address', 'line_2');
